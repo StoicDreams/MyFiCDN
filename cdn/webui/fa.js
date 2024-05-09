f4010832864853590a931d62a44968f11c5e7877
Erik Gassler <erik@gassler.us>
2024-05-09 15:19:38 -0500

    add webui-fa web component

---
+++ cdn/webui/fa.js
@@ -0,0 +1,50 @@
+/* Dynamically load font-awesome svg icons as requested */
+
+const template = document.createElement('template')
+template.innerHTML = ``;
+const faCache = {
+    'regular': {
+        'triangle-exclamation': `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Pro 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2024 Fonticons, Inc.--><path d="M27.4 432L0 480H55.3 456.7 512l-27.4-48L283.6 80.4 256 32 228.4 80.4 27.4 432zm401.9 0H82.7L256 128.7 429.3 432zM232 296v24h48V296 208H232v88zm48 104V352H232v48h48z"/></svg>`
+    }
+};
+class FontAwesome extends HTMLElement {
+    constructor() {
+        super();
+        this.svg = faCache['triangle-exclamation'];
+        this.icon = "triangle-exclamation";
+        this.family = 'regular';
+    }
+    static get observedAttributes() {
+        return ['icon', 'class'];
+    }
+    attributeChangedCallback(property, oldValue, newValue) {
+        if (oldValue === newValue) return;
+        this[property] = newValue;
+        if (property === 'icon' || property === 'family') {
+            this.updateIcon();
+        }
+    }
+    connectedCallback() {
+        const shadow = this.attachShadow({ mode: 'open' });
+        template.innerHTML = this.svg;
+        shadow.append(template);
+    }
+    disconnectedCallback() { }
+    async updateIcon() {
+        let name = this.icon;
+        let family = this.family;
+        if (!faCache[family]) {
+            faCache[family] = {};
+        }
+        if (!faCache[family][name]) {
+            let result = await fetch(`https://cdn.myfi.ws/fa/svgs/${family}/${name}.svg`);
+            if (!result.ok) return;
+            let svg = await result.text();
+            if (!svg.startsWith("<svg")) return;
+            faCache[family][name] = svg;
+        }
+
+        this.svg = faCache[family][name];
+    }
+}
+customElements.define('webui-fa', FontAwesome);
