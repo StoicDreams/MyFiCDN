d08cc335cae956024cd8af6635bc97d6c8ab7eb3
Erik Gassler <erik@gassler.us>
2024-05-09 20:14:51 -0500

    fixed webui-fa

--- cdn/webui/fa.js
+++ cdn/webui/fa.js
@@ -1,7 +1,20 @@
 /* Dynamically load font-awesome svg icons as requested */
 
 const template = document.createElement('template')
-template.innerHTML = ``;
+template.setAttribute('shadowrootmode', true);
+template.innerHTML = `
+<style type="text/css">
+    svg {
+        height: 2ch;
+        width: 3ch;
+        fill: currentColor;
+        line-height: 2ch;
+        vertical-align: middle;
+        margin-top: -0.5ch;
+    }
+</style>
+<slot name="icon"></slot>
+`;
 const faCache = {
     'regular': {
         'triangle-exclamation': `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Pro 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2024 Fonticons, Inc.--><path d="M27.4 432L0 480H55.3 456.7 512l-27.4-48L283.6 80.4 256 32 228.4 80.4 27.4 432zm401.9 0H82.7L256 128.7 429.3 432zM232 296v24h48V296 208H232v88zm48 104V352H232v48h48z"/></svg>`
@@ -10,9 +23,15 @@ const faCache = {
 class FontAwesome extends HTMLElement {
     constructor() {
         super();
-        this.svg = faCache['triangle-exclamation'];
+        this.loadid = 0;
+        this.svg = faCache['regular']['triangle-exclamation'];
         this.icon = "triangle-exclamation";
         this.family = 'regular';
+        const shadow = this.attachShadow({ mode: 'open' });
+        this.template = template.content.cloneNode(true);
+        this.iconSlot = this.template.querySelector('slot[name=icon]');
+        this.iconSlot.innerHTML = `${this.svg}`;
+        shadow.appendChild(this.template);
     }
     static get observedAttributes() {
         return ['icon', 'family', 'class'];
@@ -21,15 +40,15 @@ class FontAwesome extends HTMLElement {
         if (oldValue === newValue) return;
         this[property] = newValue;
         if (property === 'icon' || property === 'family') {
-            this.updateIcon();
+            let loadid = this.loadid + 1;
+            this.loadid = loadid;
+            setTimeout(() => {
+                if (this.loadid !== loadid) return;
+                this.updateIcon();
+            }, 1);
         }
     }
-    connectedCallback() {
-        const shadow = this.attachShadow({ mode: 'open' });
-        template.innerHTML = `Test ${this.svg}`;
-        console.log(`SVG:${this.svg}`);
-        shadow.append(template);
-    }
+    connectedCallback() { }
     disconnectedCallback() { }
     async updateIcon() {
         let name = this.icon;
@@ -45,12 +64,11 @@ class FontAwesome extends HTMLElement {
             let result = await fetch(`https://cdn.myfi.ws/fa/svgs/${family}/${name}.svg`);
             if (!result.ok) return;
             let svg = await result.text();
-            console.log(`Downloaded:${family}.${name}:${svg}`);
             if (!svg.startsWith("<svg")) return;
             faCache[family][name] = svg;
         }
         this.svg = faCache[family][name];
-        console.log(`Set SVG:${this.svg}`);
+        this.iconSlot.innerHTML = `${this.svg}`;
     }
 }
 customElements.define('webui-fa', FontAwesome);
