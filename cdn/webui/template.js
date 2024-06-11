4206c8206c0811e89144f63524739b594940465d
Erik Gassler <erik@gassler.us>
2024-06-11 11:13:06 -0500

    webui version updated

--- cdn/webui/template.js
+++ cdn/webui/template.js
@@ -1,7 +1,8 @@
 /* Template for web components. */
 "use strict"
 {
-    class Template extends HTMLElement {
+    // Start open Template
+    class OpenTemplate extends HTMLElement {
         constructor() {
             super();
             const t = this;
@@ -16,5 +17,38 @@
         connectedCallback() { }
         disconnectedCallback() { }
     }
-    customElements.define('webui-template', Template);
+    customElements.define('webui-template', OpenTemplate);
+    // End Open Template
+
+    // Start Shadow Template
+    const template = document.createElement('template')
+    template.setAttribute('shadowrootmode', true);
+    template.innerHTML = `
+<style type="text/css">
+:host {
+}
+</style>
+<slot></slot>
+<slot name="something"></slot>
+`;
+    class ShadowTemplate extends HTMLElement {
+        constructor() {
+            super();
+            const t = this;
+            const shadow = t.attachShadow({ mode: 'open' });
+            t.template = template.content.cloneNode(true);
+            shadow.appendChild(t.template);
+        }
+        static get observedAttributes() {
+            return [];
+        }
+        attributeChangedCallback(property, oldValue, newValue) {
+            if (oldValue === newValue) return;
+            this[property] = newValue;
+        }
+        connectedCallback() { }
+        disconnectedCallback() { }
+    }
+    customElements.define('webui-template', ShadowTemplate);
+    // End Shadow Template
 }
