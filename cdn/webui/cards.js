929c1e86d1f79f2cfa8f6a5761f4b0b93d79daa1
Erik Gassler <erik@gassler.us>
2024-06-18 07:58:07 -0500

    cards fix

--- cdn/webui/cards.js
+++ cdn/webui/cards.js
@@ -22,13 +22,14 @@
             cards.forEach(cd => {
                 let card = document.createElement('webui-card');
                 if (cd.theme) { card.setAttribute('theme', cd.theme); }
-                if (cd.name) { card.setAttribute('name', cd.theme); }
+                if (cd.name) { card.setAttribute('name', cd.name); }
                 if (cd.width) { card.setAttribute('width', cd.width); }
                 else if (t.cardWidth) { card.setAttribute('width', cd.cardWidth); }
                 if (cd.avatar) { card.setAttribute('avatar', cd.avatar); }
                 if (cd.link) { card.setAttribute('link', cd.link); }
                 if (cd.elevation) { card.setAttribute('elevation', cd.elevation); }
                 if (cd.class) { cd.class.split(' ').forEach(c => card.classList.add(c)); }
+                if (cd.body) { card.innerHTML = webuiApplyAppData(cd.body); }
                 t.appendChild(card);
             });
         }
