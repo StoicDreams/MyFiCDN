2176739eaf8ee2d5824ea74ebb501b73c0baa3b8
Erik Gassler <erik@gassler.us>
2024-07-14 12:35:28 -0500

    page not found updates

--- cdn/webui/page-not-found.js
+++ cdn/webui/page-not-found.js
@@ -2,16 +2,18 @@
 webui.define('webui-page-not-found', {
     connected: (t) => {
         if (!t.innerHTML) {
-            t.innerHTML = `
-<section class="paper elevation-10 side-by-side">
-<section class="paper d-flex flex-column align-center justify-center readable-content" data-subscribe="not-found" data-set="innerHTML">
-<p>The page you are looking for was not found!</p>
-</section>
-<section class="paper image d-flex flex-column align-center justify-center readable-content">
-<img src="https://cdn.myfi.ws/v/Vecteezy/404-error-illustration-exclusive-design-inspiration.svg" />
-</section>
-</section>
-`;
+            t.innerHTML = webui.trimLinePreTabs(`
+            <webui-data data-page-title="Page Not Found" data-page-subtitle="{PAGE_PATH}"></webui-data>
+            <webui-data data-page-next-page='{"name":"Home","href":"/"}'></webui-data>
+            <webui-side-by-side elevation="10">
+                <webui-flex column justify="center" data-subscribe="not-found" data-set="innerHTML">
+                    <p>The page you are looking for was not found!</p>
+                </webui-flex>
+                <webui-flex column justify="center">
+                    <img src="https://cdn.myfi.ws/v/Vecteezy/404-error-illustration-exclusive-design-inspiration.svg" />
+                </webui-flex>
+            </webui-side-by-side>
+            `);
         }
     }
 });
