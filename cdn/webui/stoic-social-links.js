ddba1e8d1b86627966efc8dcd6d8b36d541066bb
Erik Gassler <erik@gassler.us>
2024-08-19 14:51:49 -0500

    add facebook icon

--- cdn/webui/stoic-social-links.js
+++ cdn/webui/stoic-social-links.js
@@ -12,7 +12,7 @@ webui.define('webui-stoic-social-links', {
                 <webui-icon icon="discord"></webui-icon>
                 </a>
                 <a class="pa-1" title="Link to Stoic Dreams on Facebook" href="https://www.facebook.com/stoicdreams">
-                <webui-icon icon="facebook"></webui-icon>
+                <webui-icon icon="facebook|fill|shape:circle|backing"></webui-icon>
                 </a>
                 <a class="pa-1" title="Link to Stoic Dreams on Instagram" href="https://www.instagram.com/stoicdreamsllc">
                 <webui-icon icon="instagram"></webui-icon>
