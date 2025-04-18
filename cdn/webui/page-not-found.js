dad7e7487a20972e6adb0ed518f6bc37ed41af04
Erik Gassler <erik@gassler.us>
2025-04-18 18:37:29 -0500

    add support for app-not-found-html

--- cdn/webui/page-not-found.js
+++ cdn/webui/page-not-found.js
@@ -1,25 +1,33 @@
 /* Display page not found content */
-webui.define('webui-page-not-found', {
-    connected: (t) => {
-        if (!t.innerHTML) {
-            t.innerHTML = webui.trimLinePreTabs(`
-            <webui-data data-page-title="Page Not Found" data-page-subtitle="{PAGE_PATH}">
-                <template slot="json" name="page-next-page">
-                    {
-                        "name":"Home",
-                        "href":"/"
-                    }
-                </template>
-            </webui-data>
-            <webui-side-by-side elevation="10">
-                <webui-flex column justify="center" data-subscribe="app-not-found:html">
-                    <p>The page you are looking for was not found!</p>
-                </webui-flex>
-                <webui-flex column justify="center">
-                    <img src="https://cdn.myfi.ws/v/Vecteezy/404-error-illustration-exclusive-design-inspiration.svg" />
-                </webui-flex>
-            </webui-side-by-side>
-            `);
+"use strict";
+{
+    const defaultTemplate = `
+    <webui-data data-page-title="Page Not Found" data-page-subtitle="{PAGE_PATH}">
+        <template slot="json" name="page-next-page">
+            {
+                "name":"Home",
+                "href":"/"
+            }
+        </template>
+    </webui-data>
+    <webui-side-by-side elevation="10">
+        <webui-flex column justify="center" data-subscribe="app-not-found:html">
+            <p>The page you are looking for was not found!</p>
+        </webui-flex>
+        <webui-flex column justify="center">
+            <img src="https://cdn.myfi.ws/v/Vecteezy/404-error-illustration-exclusive-design-inspiration.svg" />
+        </webui-flex>
+    </webui-side-by-side>
+    `;
+    webui.define('webui-page-not-found', {
+        connected: async (t) => {
+            if (t.innerHTML) return;
+            let html = webui.getData('app-not-found-html');
+            if (html) {
+                t.innerHTML = webui.trimLinePreTabs(html);
+                return;
+            }
+            t.innerHTML = webui.trimLinePreTabs(defaultTemplate);
         }
-    }
-});
+    });
+}
--- cdn/webui/poweredby.js
+++ cdn/webui/poweredby.js
@@ -17,7 +17,7 @@ webui.define('webui-poweredby', {
     },
     connected: (t) => {
         if (!t.version) {
-            t.setAttribute("version", "0.11.13");
+            t.setAttribute("version", "0.11.14");
             webui.setData('webui-version', t.getAttribute('version'));
         }
     },
