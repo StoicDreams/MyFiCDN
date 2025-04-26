0a7a3766cbfad435c85e5631273ca6321fbe0d59
Erik Gassler <erik@gassler.us>
2025-04-26 07:22:15 -0500

    fix regex

--- cdn/webui/loader.js
+++ cdn/webui/loader.js
@@ -610,7 +610,7 @@ const webui = (() => {
         parseWebuiMarkdown(md, preTrim) {
             let t = this;
             if (typeof md !== 'string') return md;
-            md = md.replace(/(\r\n|\r)*/mg, '\n');
+            md = md.replace(/(\r\n|\r)+/mg, '\n');
             if (preTrim) {
                 md = this.trimLinePreWhitespce(md);
             } else {
