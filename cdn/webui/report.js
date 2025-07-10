edb46b614da410c1c808e8c2463fc469849a7d85
Erik Gassler <erik@gassler.us>
2025-07-10 18:07:48 -0500

    debug

--- cdn/webui/report.js
+++ cdn/webui/report.js
@@ -122,7 +122,7 @@
                 t._preSubscribe.split('|').forEach(key => {
                     key = key.split(':')[0];
                     if (key.indexOf('refresh') === 0) {
-                        console.log('clear refresh data', key);
+                        console.log('clear refresh data', key, refresh);
                         webui.setData(t._preSubscribe.split(':')[0], undefined);
                     }
                 });
