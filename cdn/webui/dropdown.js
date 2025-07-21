0e972aee7255d9ada3c9d4ad167d33a380c14a85
Erik Gassler <erik@gassler.us>
2025-07-21 11:47:00 -0500

    dropdown update

--- cdn/webui/dropdown.js
+++ cdn/webui/dropdown.js
@@ -325,7 +325,7 @@ padding:var(--padding,1rem);
 }
 webui-icon:not([icon]),
 slot[name="template"] {display:none;}
-@container container (max-width:400px) {
+@container container (max-width:600px) {
 :host {
 display:grid;
 gap:0.1em;
