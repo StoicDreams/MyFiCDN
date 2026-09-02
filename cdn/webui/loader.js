d83063a7f96ba3581501aefd16be559ba71af5b3
Erik Gassler <erikgassler@live.com>
2026-09-02 18:31:26 -0500

    add crossorigin

--- cdn/webui/loader.js
+++ cdn/webui/loader.js
@@ -2831,6 +2831,7 @@ const webui = (() => {
             let script = webui.create('script');
             script.setAttribute('async', true);
             script.setAttribute('src', `${wcRoot}webui/${wc}${wcMin}.js`);
+            script.setAttribute('crossorigin', 'anonymous');
             script.onload = () => {
                 wcLoaded[wc] = true;
                 resolve();
@@ -2853,6 +2854,7 @@ const webui = (() => {
             let script = webui.create('script');
             script.setAttribute('async', true);
             script.setAttribute('src', `${webui.appSrc}/${wc}${webui.appMin}.js`);
+            script.setAttribute('crossorigin', 'anonymous');
             script.onload = () => {
                 appLoaded[wc] = true;
                 resolve();
