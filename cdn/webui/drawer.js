4f8820ed67d50a4a95e8288d2faae7be50b0a1fc
Erik Gassler <erikgassler@live.com>
2026-09-12 18:56:04 -0500

    test

--- cdn/webui/drawer.js
+++ cdn/webui/drawer.js
@@ -90,7 +90,7 @@
             if (this.dataDockable && !this._forceUndocked) { content += dockableTemplate.split('[ID]').join(this._idselector); }
             if (fb.length === 1 && content === t._footerContent) return;
             t._footerContent = content;
-            console.log('update footer', content);
+            console.log('update footer', fb.length, content);
             fb.forEach(el => el.remove());
             fb = webui.create('webui-flex');
             fb.setAttribute('justify', 'center');
